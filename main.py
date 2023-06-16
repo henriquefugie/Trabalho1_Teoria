7a8eab9445c1d3bb3061f3afd33e4b7092212274
joaovitordf <xjoaovitordf@gmail.com>
2023-06-16 03:04:54 -0300

    head e break implementados

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 
 # Joao Vitor Dias Fernandes e Henrique Fugie
@@ -12,7 +13,7 @@ class NoPilha:
         return '%s -> %s' % (self.dado, self.anterior)
 
 class MT:
-    def __init__(self, input, arquivo):
+    def __init__(self, input, arquivo, delimitador):
         self.simbolo = input[0]
         input = ''.join(['_____________________']) + input + \
             ''.join(['____________________'])
@@ -28,6 +29,7 @@ class MT:
         self.linhasArquivo_array = arquivo.splitlines()
         self.topo_pilha = None
         self.conta_passos = 0
+        self.delimitador = delimitador
         while self.fimArquivo:
             if self.linha_atual == len(self.linhasArquivo_array):
                 self.fimArquivo = False
@@ -87,6 +89,7 @@ class MT:
                             self.bloco_linha[1], novo_simbolo, movimento, novo_estado)
                     # Caso breakpoint
                     if len(valores_linha) == 7:
+                        estado_atual, simbolo_atual, traco, novo_simbolo, movimento, novo_estado, breakpoint = valores_linha
                         self.dicionario[self.bloco_linha[0], estado_atual, simbolo_atual] = (
                             self.bloco_linha[1], novo_simbolo, movimento, novo_estado, '!')
                 self.linha_atual += 1
@@ -94,7 +97,20 @@ class MT:
     # Responsavel por executar uma linha de comando do arquivo
     # Exemplo: 1 _ -- * d 2
     def executa_linha(self, dados_linha):
-        linha, novo_simbolo, movimento, novo_estado = dados_linha
+        if len(dados_linha) == 4:
+            linha, novo_simbolo, movimento, novo_estado = dados_linha
+        elif len(dados_linha) == 5:
+            linha, novo_simbolo, movimento, novo_estado, breakpoint = dados_linha
+        """print(self.dicionario)
+        if novo_estado != '*' and novo_simbolo != '*':
+            chave = (self.bloco, str(self.estado), self.simbolo)
+            if chave in self.dicionario:
+                dado = self.dicionario[chave]
+                # dado[1] = novo simbolo
+                # dado[3] = novo estado
+                if dado[1] != novo_simbolo or dado[3] != novo_estado:
+                    print("Erro")
+                    sys.exit("Programa encerrado devido a um erro")"""
         if novo_estado != '*':
             self.estado = novo_estado
         if novo_simbolo != '*':
@@ -131,31 +147,30 @@ class MT:
     def passo(self, bloco, estado, simbolo, args, passo_limite):
         if args.step:
             if passo_limite > 0 and self.conta_passos <= passo_limite:
-                self.mostra_fita()
                 self.conta_passos += 1
-        elif args.verbose:
-            self.mostra_fita()
         if str(estado) != 'pare':
             dados_linha = self.chave_dicionario(bloco, estado, simbolo)
-
-            if len(dados_linha) == 4:
-                self.executa_linha(dados_linha)
             # Breakpoint
-            elif len(dados_linha) == 5:
-                return
-            else:
-                bloco_retorno = self.bloco
-                estado_retorno, bloco_atual, estado_inicial = dados_linha
-                self.bloco = bloco_atual
-                bloco, estado, simbolo, estado_retorno, bloco_retorno
-                self.insere_pilha((bloco_atual, estado_inicial,
-                                self.fita[self.cabeca], estado_retorno, bloco_retorno))
-                
-                dados = self.topo_pilha.dado
+            if len(dados_linha) != 5:
+                if len(dados_linha) == 4:
+                    self.executa_linha(dados_linha)
+                else:
+                    bloco_retorno = self.bloco
+                    estado_retorno, bloco_atual, estado_inicial = dados_linha
+                    self.bloco = bloco_atual
+                    bloco, estado, simbolo, estado_retorno, bloco_retorno
+                    self.insere_pilha((bloco_atual, estado_inicial,
+                                    self.fita[self.cabeca], estado_retorno, bloco_retorno))
+                    
+                    dados = self.topo_pilha.dado
 
-                # Essa linha eh responsavel por checar qual o estado inicial do bloco chamado e atualizar o self.estado com esse estado inicial
-                self.estado = self.inicio_bloco[dados[0]]
-                self.passo_bloco(dados[0], self.estado, dados[2], dados[3], dados[4], args, passo_limite)
+                    # Essa linha eh responsavel por checar qual o estado inicial do bloco chamado e atualizar o self.estado com esse estado inicial
+                    self.estado = self.inicio_bloco[dados[0]]
+                    self.passo_bloco(dados[0], self.estado, dados[2], dados[3], dados[4], args, passo_limite)
+            else:
+                self.executa_linha(dados_linha)
+                return 1
+        return 0
 
     def passo_bloco(self, bloco, estado, simbolo, estado_retorno, bloco_retorno, args, passo_limite):
         if args.step:
@@ -166,60 +181,68 @@ class MT:
             self.mostra_fita()
         if str(estado) != 'pare':
             dados_linha = self.chave_dicionario(bloco, estado, simbolo)
-            
-            if len(dados_linha) == 4:
-                if dados_linha[3] != 'retorne':
-                    self.executa_linha(dados_linha)
-                    self.passo_bloco(
-                        bloco, self.estado, self.simbolo, estado_retorno, bloco_retorno, args, passo_limite)
-                else:
-                    self.remove_pilha()
-                    # Retornar para o bloco anterior
-                    self.executa_linha(dados_linha)
-                    if self.topo_pilha is not None:
-                        if self.topo_pilha.anterior == None:
-                            self.bloco = self.topo_pilha.dado[4]
-                            self.estado = self.topo_pilha.dado[3]
-                            self.passo(self.bloco, self.estado,
-                                       self.fita[self.cabeca], args, passo_limite)
-                        else:
-                            dados = self.topo_pilha.dado
-                            self.passo_bloco(
-                                dados[0], dados[1], dados[2], dados[3], dados[4], args, passo_limite)
+            if len(dados_linha) != 5:
+                # Breakpoint
+                if len(dados_linha) == 4:
+                    if dados_linha[3] != 'retorne':
+                        self.executa_linha(dados_linha)
+                        self.passo_bloco(
+                            bloco, self.estado, self.simbolo, estado_retorno, bloco_retorno, args, passo_limite)
                     else:
-                        self.bloco = bloco_retorno
-                        self.estado = estado_retorno
-                        self.passo(bloco_retorno, estado_retorno, self.simbolo, args, passo_limite)
-            # Breakpoint
-            elif len(dados_linha) == 5:
-                return
-            else:
-                # Caso de bloco sendo chamado dentro de outro bloco
-                bloco_retorno = self.bloco
-                estado_retorno, bloco_atual, estado_inicial = dados_linha
-                self.bloco = bloco_atual
-                bloco, estado, simbolo, estado_retorno, bloco_retorno
-                self.insere_pilha(
-                    (bloco_atual, estado_inicial, self.fita[self.cabeca], estado_retorno, bloco_retorno))
-                dados = self.topo_pilha.dado
+                        self.remove_pilha()
+                        # Retornar para o bloco anterior
+                        self.executa_linha(dados_linha)
+                        if self.topo_pilha is not None:
+                            if self.topo_pilha.anterior == None:
+                                self.bloco = self.topo_pilha.dado[4]
+                                self.estado = self.topo_pilha.dado[3]
+                                self.passo(self.bloco, self.estado,
+                                        self.fita[self.cabeca], args, passo_limite)
+                            else:
+                                dados = self.topo_pilha.dado
+                                self.passo_bloco(
+                                    dados[0], dados[1], dados[2], dados[3], dados[4], args, passo_limite)
+                        else:
+                            self.bloco = bloco_retorno
+                            self.estado = estado_retorno
+                            self.passo(bloco_retorno, estado_retorno, self.simbolo, args, passo_limite)
+                else:
+                    # Caso de bloco sendo chamado dentro de outro bloco
+                    bloco_retorno = self.bloco
+                    estado_retorno, bloco_atual, estado_inicial = dados_linha
+                    self.bloco = bloco_atual
+                    bloco, estado, simbolo, estado_retorno, bloco_retorno
+                    self.insere_pilha(
+                        (bloco_atual, estado_inicial, self.fita[self.cabeca], estado_retorno, bloco_retorno))
+                    dados = self.topo_pilha.dado
 
-                # Essa linha eh responsavel por checar qual o estado inicial do bloco chamado e atualizar o self.estado com esse estado inicial
-                self.estado = self.inicio_bloco[dados[0]]
-                self.passo_bloco(dados[0], self.estado,
-                                 dados[2], dados[3], dados[4], args, passo_limite)
+                    # Essa linha eh responsavel por checar qual o estado inicial do bloco chamado e atualizar o self.estado com esse estado inicial
+                    self.estado = self.inicio_bloco[dados[0]]
+                    self.passo_bloco(dados[0], self.estado,
+                                    dados[2], dados[3], dados[4], args, passo_limite)
+            else:
+                self.executa_linha(dados_linha)
+                return 1
+        return 0
 
     def programa(self, args, passo_limite):
         erro = 0
-        while self.estado != 'pare' and erro < 100000000:
-            self.passo(self.bloco, self.estado, self.simbolo, args, passo_limite)
+        verifica_break = -1
+        while self.estado != 'pare' and erro < 100000000 and verifica_break != 1 and erro < passo_limite:
+            verifica_break = self.passo(self.bloco, self.estado, self.simbolo, args, passo_limite)
+            if not args.resume:
+                self.mostra_fita()
+            #print(self.estado)
             erro += 1
-        if args.verbose:
-            self.mostra_fita()
+        return verifica_break
 
     def mostra_fita(self):
+        delim = self.delimitador[0]
+        token_esquerda = delim[0]
+        token_direita = delim[1]
         elementos_esquerda = self.fita[max(0, self.cabeca-20):self.cabeca]
         elementos_direita = self.fita[self.cabeca+1:self.cabeca+21]
-        nova_lista = elementos_esquerda + ['(' + self.fita[self.cabeca] + ')'] + elementos_direita
+        nova_lista = elementos_esquerda + [token_esquerda + self.fita[self.cabeca] + token_direita] + elementos_direita
         elementos = ''.join(nova_lista)
         print(self.bloco, self.estado, elementos)
 
@@ -245,34 +268,68 @@ def remove_tabulacao(nome_arquivo):
         return conteudo_sem_tab
 
 def main():
-    parser = argparse.ArgumentParser(description='Simulador de Máquina de Turing')
-    parser.add_argument('programa', help='Nome do arquivo que contém o código da MT')
-    parser.add_argument('-verbose', '-v', action='store_true', help='Mostra a execução passo a passo do programa até o fim')
-    parser.add_argument('-step', '-s', type=int, help='Mostra n linhas de execução passo a passo na tela')
-    parser.add_argument('-resume', '-r', action='store_true', help='Executa o programa até o fim e imprime o conteúdo final na fita')
-    args = parser.parse_args()
-    print('Simulador de Máquina de Turing v1.0 - IFMG 2023')
-    print('Desenvolvido como trabalho prático para a disciplina de Teoria da Computação')
-    print('Autores: Joao Vitor Dias Fernandes & Henrique Fugie\n')
-    palavra_inicial = input('Forneça a palavra inicial: ')
-    step_value = args.step
-    programa_value = 'tp2etestes/'
-    programa_value += args.programa
-    arquivo_sem_tab1 = remove_tabulacao(programa_value)
+    breakpoint = -1
+    execucao_inicial = True
+    while True:
+        if execucao_inicial == True:
+            parser = argparse.ArgumentParser(description='Simulador de Máquina de Turing')
+            parser.add_argument('programa', help='Nome do arquivo que contém o código da MT')
+            parser.add_argument('-verbose', '-v', action='store_true', help='Mostra a execução passo a passo do programa até o fim')
+            parser.add_argument('-step', '-s', type=int, help='Mostra n linhas de execução passo a passo na tela')
+            parser.add_argument('-resume', '-r', action='store_true', help='Executa o programa até o fim e imprime o conteúdo final na fita')
+            parser.add_argument('-head', metavar='<>', nargs=1, help='Descrição da opção -head')
+            args = parser.parse_args()
+            print('Simulador de Máquina de Turing v1.0 - IFMG 2023')
+            print('Desenvolvido como trabalho prático para a disciplina de Teoria da Computação')
+            print('Autores: Joao Vitor Dias Fernandes & Henrique Fugie\n')
+            step_value = args.step
 
-    mt = MT(palavra_inicial, arquivo_sem_tab1)
-    if args.verbose:
-        print("Executando a opcao verbose")
-        mt.programa(args, -1)
-    if args.step:
-        print("Executando a opcao step")
-        print("Quantidade de steps:", step_value)
-        mt.programa(args, step_value)
-    if args.resume:
-        print("Executando a opcao resume")
-        mt.programa(args, -1)
-        elementos = ''.join(mt.fita)
-        print(elementos)
+        if execucao_inicial == False:
+            aux = args
+            opcao = input("Forneça opção ( r , v , s ) : ")
+            if opcao == 'r':
+                args.verbose = False
+                args.step = False
+                args.resume = True
+            elif opcao == 'v':
+                args.step = False
+                args.resume = False
+                args.verbose = True
+            elif opcao == 's':
+                args.verbose = False
+                args.resume = False
+                args.step = True
+                step_value = int(input("Quantidade de passos: "))
+            # Apertou enter
+            elif opcao == '':
+                args = aux
+        if breakpoint != 1:
+            palavra_inicial = input('Forneça a palavra inicial: ')
+            programa_value = 'tp2etestes/'
+            programa_value += args.programa
+            arquivo_sem_tab1 = remove_tabulacao(programa_value)
+            if args.head:
+                delimitador = args.head[0].split()
+            else:
+                delimitador = ['()']
+            mt = MT(palavra_inicial, arquivo_sem_tab1,delimitador)
+        if args.verbose:
+            print("Executando a opcao verbose")
+            breakpoint = mt.programa(args, 1000000)
+            if breakpoint != 1:
+                return
+        if args.step:
+            print("Executando a opcao step")
+            print("Quantidade de steps:", step_value)
+            mt.programa(args, step_value)
+            mt.conta_passos = 0
+        if args.resume:
+            print("Executando a opcao resume")
+            breakpoint = mt.programa(args, 1000000)
+            elementos = ''.join(mt.fita)
+            print(elementos)
+
+        execucao_inicial = False
 
 if __name__ == "__main__":
     main()
